64ae0cedd4b285db32c877e369b1150ea1fc69a7
fbritogambetta <britogambettafranco@gmail.com>
2021-08-16 16:06:08 -0300

    Adelanto
    Cree login y trate de establecer el js

--- js/login.js
+++ js/login.js
@@ -1,6 +1,14 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+let validacionTerminado = () => {
+    document.getElementById("redireccionALogin").remove;
+    window.location.href="index.html";
+}
+//eliminar el elemento meta que redirecciona a login en el index.html
 
+document.addEventListener("DOMContentLoaded", function(e){
+    document.getElementById("submitInformation").onclick = () => {
+        validacionTerminado();
+    }
 });
