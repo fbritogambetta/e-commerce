538bb22031de23617d5aba8f542f59a9b24b5ac3
fbritogambetta <britogambettafranco@gmail.com>
2021-08-20 13:14:57 -0300

    Pronto
    lista de productos y login hecho

--- js/init.js
+++ js/init.js
@@ -43,10 +43,9 @@ var getJSONData = function(url){
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-const miStorage = window.localStorage
 
 const checkLogin = () =>  {
-  if((miStorage.getItem("loged")) != true) {
+  if(!sessionStorage.getItem("loged")) {
     window.location = "login.html"
   }
 }
--- js/login.js
+++ js/login.js
@@ -1,15 +1,12 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-const boton = document.getElementById("submitInformation")
-const mail = document.getElementById("userEmail").value
-const password = document.getElementById("password").value
-const miStorage = window.localStorage
 
 const loged = () =>  {
-    miStorage.setItem("loged", true);
-    window.location = "index.html";
+    sessionStorage.setItem("loged", true);
+    window.location="index.html";
 }
+
 document.addEventListener("DOMContentLoaded", function(e){
-    boton.onclick = loged()
+    document.getElementById("submitInformation").addEventListener("click", loged)
 });
