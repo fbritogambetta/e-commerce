5d9b3fa415a7c1eb916f6ad3b140f49f586690ae
fbritogambetta <britogambettafranco@gmail.com>
2021-08-30 18:37:29 -0300

    Actualización
    Avance segunda entrega

--- js/products.js
+++ js/products.js
@@ -1,15 +1,70 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-const url = "https://japdevdep.github.io/ecommerce-api/product/all.json"
-const cacillas = document.getElementById("productListInformation")
+const cargarLista = () => {
+    fetch(PRODUCTS_URL)
+        .then(data => data.json())
+        .then(data => {
+            filtrarOrdenar(data)
+    })
+}
+function buscador(array) {
+    let parametroDeBusqueda = document.getElementById("buscador").value;
+    let text = parametroDeBusqueda.toLowerCase()
+    let newArray = []
+    for(let i of array) {
+        if(i.name.includes(text) || i.description.includes(text)) {
+            newArray.push(i)
+        }
+    }
+    tabla(newArray)
+}
 
-function filtrarOrdenar(valor)  {
-    const eleccion = document.getElementById("ordenarPor").value
-    
+function filtrarOrdenar(array)  {
+    const criterio = document.querySelector("select").value;
+    const pMin = document.getElementById("precioMin").value;
+    const pMax = document.getElementById("precioMax").value
+    if(criterio === "asc") {array.sort((a, b) => {
+        if(a.cost > b.cost) return 1;
+        if(a.cost < b.cost) return -1;
+        return 0;})
+    }
+    if(criterio === "desc") {array.sort((a, b) => {
+        if(a.cost > b.cost) return -1;
+        if(a.cost < b.cost) return 1;
+        return 0;})
+    }
+    if(criterio === "masV") {array.sort((a, b) => {
+        if(a.soldCount > b.soldCount) return -1;
+        if(a.soldCount < b.soldCount) return 1;
+        return 0;})
+    }
+    if(criterio === "menV") {array.sort((a, b) => {
+        if(a.soldCount > b.soldCount) return 1;
+        if(a.soldCount < b.soldCount) return -1;
+        return 0;})
+    }
+    if(criterio === "az") {array.sort((a,b) => a.name.localeCompare(b.name))
+    }
+    if(criterio === "za") {array.sort((a,b) => b.name.localeCompare(a.name))
+    }
+    if(pMin !== "" || pMax !== "") {
+        let newArray = []
+        for(let i of array) {
+            if(i.cost >= pMin && i.cost <= pMax) {
+                newArray.push(i)
+            }
+            buscador(newArray)
+        }
+    }
+    if(pMin === "" && pMax === "") {
+        buscador(array)
+    }
 }
 
 function tabla(alpha) {
+    const cacillas = document.getElementById("productListInformation");
+    cacillas.innerHTML =``
     for(let valor of alpha)  {
         cacillas.innerHTML += `
         <tr>
@@ -23,10 +78,9 @@ function tabla(alpha) {
 }
 
 document.addEventListener("DOMContentLoaded", function (e) {
-    fetch(url)
-        .then(data => data.json())
-        .then(data => {
-            tabla(data)
-        })
-        
+    cargarLista();
+    document.getElementById("ordenarPor").addEventListener("change", cargarLista);
+    document.getElementById("precioMin").addEventListener("keypress", cargarLista)
+    document.getElementById("precioMax").addEventListener("keypress", cargarLista)
+    document.getElementById("buscador").addEventListener("keypress", cargarLista)
 });
