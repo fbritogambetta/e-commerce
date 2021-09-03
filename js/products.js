fb390c9695bb0679d23d7bc5d0ef5d523228e7b7
fbritogambetta <britogambettafranco@gmail.com>
2021-09-03 16:34:58 -0300

    Actualizacion entregable  2
    filtrar, ordenar y buscador aplicados

--- js/products.js
+++ js/products.js
@@ -5,59 +5,74 @@ const cargarLista = () => {
     fetch(PRODUCTS_URL)
         .then(data => data.json())
         .then(data => {
-            filtrarOrdenar(data)
-    })
+            buscador(data)
+        })
 }
-const filtrarPorPrecio = (array,min,max) => {
-    tabla(array.filter(valor=> valor.cost >= min && valor.cost <= max));
+const filtrarPorPrecio = (array, min, max) => {
+    tabla(array.filter(valor => valor.cost >= min && valor.cost <= max));
 }
 function buscador(array) {
-    let parametroDeBusqueda = document.getElementById("buscador").value;
-    let text = parametroDeBusqueda.toLowerCase()
-    tabla(array.filter(value => value.includes(text)))
+    let newArray =  []
+    const valor = document.getElementById("buscador");
+    const parametro = valor.value.toLowerCase();
+    for(let element of array){
+        let nombre = element.name.toLowerCase();
+        let description = element.description.toLowerCase()
+        if(nombre.indexOf(parametro) !== -1 || description.indexOf(parametro) !== -1){
+            newArray.push(element)}
+
+    }
+    filtrarOrdenar(newArray)
 }
 
-function filtrarOrdenar(array)  {
+function filtrarOrdenar(array) {
     const criterio = document.querySelector("select").value;
     const min = document.getElementById("precioMin").value;
     const max = document.getElementById("precioMax").value;
-    if(criterio === "asc") {array.sort((a, b) => {
-        if(a.cost > b.cost) return 1;
-        if(a.cost < b.cost) return -1;
-        return 0;})
+    if (criterio === "asc") {
+        array.sort((a, b) => {
+            if (a.cost > b.cost) return 1;
+            if (a.cost < b.cost) return -1;
+            return 0;
+        })
     }
-    if(criterio === "desc") {array.sort((a, b) => {
-        if(a.cost > b.cost) return -1;
-        if(a.cost < b.cost) return 1;
-        return 0;})
+    if (criterio === "desc") {
+        array.sort((a, b) => {
+            if (a.cost > b.cost) return -1;
+            if (a.cost < b.cost) return 1;
+            return 0;
+        })
     }
-    if(criterio === "masV") {array.sort((a, b) => {
-        if(a.soldCount > b.soldCount) return -1;
-        if(a.soldCount < b.soldCount) return 1;
-        return 0;})
+    if (criterio === "masV") {
+        array.sort((a, b) => {
+            if (a.soldCount > b.soldCount) return -1;
+            if (a.soldCount < b.soldCount) return 1;
+            return 0;
+        })
     }
-    if(criterio === "menV") {array.sort((a, b) => {
-        if(a.soldCount > b.soldCount) return 1;
-        if(a.soldCount < b.soldCount) return -1;
-        return 0;})
+    if (criterio === "menV") {
+        array.sort((a, b) => {
+            if (a.soldCount > b.soldCount) return 1;
+            if (a.soldCount < b.soldCount) return -1;
+            return 0;
+        })
     }
-    if(criterio === "az") array.sort((a,b) => a.name.localeCompare(b.name));
-    if(criterio === "za") array.sort((a,b) => b.name.localeCompare(a.name));
-    filtrarPorPrecio(array,min,max)
+    if (criterio === "az") array.sort((a, b) => a.name.localeCompare(b.name));
+    if (criterio === "za") array.sort((a, b) => b.name.localeCompare(a.name));
+    filtrarPorPrecio(array, min, max)
 }
 
 function tabla(alpha) {
     const casillas = document.getElementById("productListInformation");
-    casillas.innerHTML =``
-    for(let valor of alpha)  {
+    casillas.innerHTML = ``
+    for (let valor of alpha) {
         casillas.innerHTML += `
         <tr>
-          <td> <img class="imgRedonda" src="${ valor.imgSrc }"></td>
-          <td>${ valor.name }</td>
-          <td>${ valor.description }</td>
-          <td>${ valor.cost }` + `(${ valor.currency})</td> 
-        </tr>
-        `
+        <td><img class="imgRedonda" src="${valor.imgSrc}"></td>
+        <td>${valor.name}</td>
+        <td>${valor.description}</td>
+        <td>${valor.cost}` + `(${valor.currency})</td> 
+        </tr>`
     }
 }
 
@@ -66,5 +81,5 @@ document.addEventListener("DOMContentLoaded", function (e) {
     document.getElementById("ordenarPor").addEventListener("change", cargarLista);
     document.getElementById("precioMin").addEventListener("keypress", cargarLista)
     document.getElementById("precioMax").addEventListener("keypress", cargarLista)
-    //document.getElementById("buscador").addEventListener("keypress", buscador())
+    document.getElementById("buscador").addEventListener("keypress", cargarLista)
 });
