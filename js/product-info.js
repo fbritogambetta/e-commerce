3096efd9d345b379b574be83bc2105f8cfbc8c9e
fbritogambetta <88290419+fbritogambetta@users.noreply.github.com>
2021-10-03 10:03:43 -0300

    Avance entrega 4
    pauta individual completa

--- js/product-info.js
+++ js/product-info.js
@@ -1,9 +1,10 @@
 const showInfo = (array) => {
     document.getElementById("name").innerHTML = array.name;
-    document.getElementById("description").innerHTML = array.description
+    document.getElementById("description").innerHTML = array.description;
     document.getElementById("price").innerHTML = 'Precio: ' + array.cost + ' ' + array.currency;
     document.getElementById("category").innerHTML = 'Categoria: ' + array.category + '/Cantidad de ventas: ' + array.soldCount;
-    showImgs(array)
+    showImgs(array);
+    showRelatedProducts(array);
 }
 const showImgs = (array) => {
     const imgsLocation = array.images;
@@ -12,8 +13,33 @@ const showImgs = (array) => {
         imgs.innerHTML += '<img class="img-fluid img-thumbnail desplegar" src="' + img + '">';
     }
 }
-const showProductInfAndImg = () => {
-    fetch(PRODUCT_INFO_URL)
+const showRelatedProducts = (array) => {
+    fetch(PRODUCTS_URL)
+        .then(data => data.json())
+        .then(data => {
+            printCardOfArray(array, data)
+        })
+    
+}
+const printCardOfArray = (array1, array2) => {
+    for(let value of array1.relatedProducts) {
+        let = arrayElement = array2[value];
+        printCard(arrayElement)
+    }
+}
+const printCard = (array) => {
+    document.getElementById("relatedProducts").innerHTML+=`
+    <div class="card" style="width: 18rem;">
+    <img src="${array.imgSrc}" class="card-img-top">
+    <div class="card-body">
+      <h5 class="card-title">${array.name}</h5>
+      <p class="card-text">${array.description}</p>
+      <a class="btn btn-primary">(${array.currency})${array.cost}</a>
+    </div>
+  </div>`
+}
+const showProductInfAndImg = (array) => {
+    fetch(array)
         .then(data => data.json())
         .then(data => {
             showInfo(data)
@@ -24,8 +50,8 @@ const showComments = (array) => {
         document.getElementById("score").innerHTML += '<h6>' + valor.user + '</h6><p>Puntuación: ' + valor.score + '</p><p>' + valor.description + '</p><hr>'
     }
 }
-const showScoreAndComments = () => {
-    fetch(PRODUCT_INFO_COMMENTS_URL)
+const showScoreAndComments = (array) => {
+    fetch(array)
         .then(data => data.json())
         .then(data => {
             showComments(data)
@@ -35,6 +61,6 @@ const showScoreAndComments = () => {
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
-    showProductInfAndImg();
-    showScoreAndComments();
+    showProductInfAndImg(PRODUCT_INFO_URL);
+    showScoreAndComments(PRODUCT_INFO_COMMENTS_URL);
 });
