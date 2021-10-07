a961c1e55a3c4ea49295364f05fd8eb6a01ef70a
fbritogambetta <88290419+fbritogambetta@users.noreply.github.com>
2021-10-07 10:14:58 -0300

    Avance entrega 5

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,28 @@
+let total = 0
+const printProducts =(array)=>{
+    const casillas = document.getElementById("productListInformation");
+    for (let valor of array.articles) {
+        casillas.innerHTML+=`
+        <tr>
+        <td><img class="imgRedonda" src="${valor.src}"></td>
+        <td>${valor.name}</td>
+        <td>${valor.unitCost} (${valor.currency})</td>
+        <td><input  type="number" id="precioMin" size="6" value="${valor.count}"></td>
+        <td>${valor.unitCost*valor.count} (${valor.currency})</td>
+        </tr>`
+        if(valor.currency==="UYU") total += valor.unitCost*valor.count;
+    }
+}
+const showCartProducts= (array) => {
+    fetch(array)
+        .then(data => data.json())
+        .then(data => {
+            printProducts(data)
+        })
+}
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
-
+    showCartProducts(CART_INFO_URL)
 });
